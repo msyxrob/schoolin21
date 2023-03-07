4b0795e919119f57a83934accafdeb87ca29f6bc
msyxrob <m.syxrob-1820>
2023-03-07 22:54:00 +0500

    schoolin21 with some errors #2

--- school_management_system/main/urls.py
+++ school_management_system/main/urls.py
@@ -16,4 +16,6 @@ app_name = 'main'
 urlpatterns = [
     path('', views.index, name='index'),
     path('class/<int:pk>/', views.class_detail, name='class_detail'),
+    path('detail/', views.class_detail, name='detail'),
+
 ]
