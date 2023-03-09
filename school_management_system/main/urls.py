30017d4b4ac2c7296e520a80dd8096e5db4c77a8
msyxrob <m.syxrob-1820>
2023-03-10 00:28:39 +0500

    schoolin21 with some errors #3

--- school_management_system/main/urls.py
+++ school_management_system/main/urls.py
@@ -1,13 +1,4 @@
-# from django.urls import path
-# from . import views
-#
-# app_name = 'main'
-#
-# urlpatterns = [
-#     path('', views.index, name='index'),
-#     path('class/<int:pk>/', views.class_detail, name='class_detail'),
-#     path('student/<int:pk>/', views.StudentDetailView.as_view(), name='student_detail'),
-# ]
+
 from django.urls import path
 
 from . import views
